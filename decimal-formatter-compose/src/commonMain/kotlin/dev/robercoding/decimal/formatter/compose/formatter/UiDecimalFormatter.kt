bbe88bd8acf224d9878b9c85d6a116fdc2f702d8
robercoding <robercoding@gmail.com>
2025-07-29 17:25:50 +0200

    Add UiDecimalFormatter for enhanced decimal value formatting

---
+++ decimal-formatter-compose/src/commonMain/kotlin/dev/robercoding/decimal/formatter/compose/formatter/UiDecimalFormatter.kt
@@ -0,0 +1,29 @@
+package dev.robercoding.decimal.formatter.compose.formatter
+
+import dev.robercoding.decimal.formatter.compose.model.DecimalValue
+import dev.robercoding.decimal.formatter.core.DecimalFormatter
+import dev.robercoding.decimal.formatter.core.DecimalFormatterConfiguration
+
+data class UiDecimalFormatter(
+    private val decimalFormatter: DecimalFormatter = DecimalFormatter.DefaultFormatter,
+    private val prefix: String? = null
+) {
+    companion object {
+        public val DefaultUiFormatter: UiDecimalFormatter
+            get() = UiDecimalFormatter(DecimalFormatter.DefaultFormatter, null)
+    }
+    fun format(value: String): DecimalValue {
+        val rawDigits = decimalFormatter.getRawDigits(value)
+        val display = decimalFormatter.format(rawDigits)
+        val fullDisplay = prefix?.takeIf { it.isNotBlank() }?.let { "$it$display" }
+        return DecimalValue(rawDigits, display, fullDisplay)
+    }
+    
+    fun format(value: Double): DecimalValue = format(value.toString())
+
+    fun format(value: Float): DecimalValue = format(value.toString())
+
+    fun format(value: Long): DecimalValue = format(value.toString())
+
+    fun format(value: Int): DecimalValue = format(value.toString())
+}
